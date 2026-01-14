45428f7bcb672635ae16ca40a752f9722730cb01
Minyong.Ha <hajoel597@gmail.com>
2026-01-14 12:55:30 +0900

    modified:   style.css 	modified:   template-parts/content-sub.php

--- template-parts/content-sub.php
+++ template-parts/content-sub.php
@@ -1,6 +1,8 @@
 <div class="home-list-muilt-container">
     <div class="image">
-        <?php if ( has_post_thumbnail() ) { the_post_thumbnail( 'medium' ); } ?>
+        <a href="<?php the_permalink(); ?>">
+            <?php if ( has_post_thumbnail() ) { the_post_thumbnail( 'medium' ); } ?>
+        </a>
         <h4 class="category"><?php the_category( ', ' ); ?></h4>
     </div>
     <div class="desc">
@@ -16,7 +18,6 @@
                 if ( empty( $excerpt ) ) {
                     echo '<p class="excerpt">요약문 준비 중</p>';
                 } else {
-                    // get_the_excerpt()로 내용을 가져온 뒤 직접 p 태그로 감싸며 클래스 부여
                     echo '<p class="excerpt">' . $excerpt . '</p>';
                 }
                 ?>
