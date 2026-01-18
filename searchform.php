e5701a96663fde010907c985895b4880aff47071
hajoel597 <78016278+hajoel597@users.noreply.github.com>
2026-01-18 17:53:28 +0900

    Delete searchform.php

--- searchform.php
+++
@@ -1,7 +0,0 @@
-<form role="search" method="get" class="search-form" action="<?php echo home_url( '/' ); ?>">
-    <span class="panel-search-icon material-symbols-outlined">
-        search
-    </span>
-    <input type="search" class="search-field" placeholder="검색어 입력" value="<?php echo get_search_query(); ?>" name="s" />
-
-</form>
