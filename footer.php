34a751eaff8b58b305444da484753cb54f53932c
Minyong.Ha <hajoel597@gmail.com>
2026-02-20 09:34:21 +0900

    modified:   footer.php

--- footer.php
+++ footer.php
@@ -3,6 +3,7 @@
         <div class="footer-left">
             <p>&copy; <?php echo date('Y'); ?> <?php bloginfo('name'); ?>.</p>
         </div>
+        <a href="<?php echo esc_url( home_url( '/' ) ); ?>">홈으로</a>
         <div class="footer-right">
             <a href="#" class="top-button">Back to Top</a>
         </div>
